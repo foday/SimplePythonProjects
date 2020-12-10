8891936107533e1691b960fec0378fbba4226524
foday <foday_s@yahoo.com>
2020-12-09 19:21:40 -0600

    About Printing guess result
    Note: I couldn't get an output until i typed 'guess(0)'

--- Computer_Guess_Number.py
+++ Computer_Guess_Number.py
@@ -1,6 +1,10 @@
 import random 
 
 def guess(x):
-    random_number = random.randit(1, x)
+    random_number = random.randint(1, x)
     guess = 0
-    while guess != random_number
+    while guess != random_number:
+        guess = input(f'Guess a number between 1 and {x}:')
+        print(guess)
+
+guess(10)
